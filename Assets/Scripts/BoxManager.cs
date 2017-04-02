cb4229b43c3d553f81da56675afd790fa9c8e805
Steven <yourgamesbeover@gmail.com>
2017-04-02 18:03:03 -0400

    Added ability to move the table

---
+++ Assets/Scripts/BoxManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PsychImmersion
+{
+    public class BoxManager : MonoBehaviour
+    {
+        private BoxState _curState;
+
+        public enum BoxState
+        {
+            Hidden,
+            Normal,
+            Opened
+        }
+
+        public float MoveSpeed = 0.1f;
+        //public AnimationCurve MovementSpeedByDistanceCurve = new AnimationCurve(new Keyframe(0f,1f), new Keyframe(1f,1f));
+
+        private float _targetDistanceFromPlayer;
+        public float DistanceFromPlayer
+        {
+            get { return _targetDistanceFromPlayer;}
+            set { SetDistanceFromPlayer(value); }
+        }
+
+        private bool _boxIsMoving = false;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _curState = BoxState.Hidden;
+            _targetDistanceFromPlayer = transform.position.z;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+
+        public void TransitionToState(BoxState state)
+        {
+            _curState = state;
+        }
+
+        public void SetDistanceFromPlayer(float newDistance)
+        {
+            _targetDistanceFromPlayer = newDistance;
+            if (!_boxIsMoving)
+            {
+                StartCoroutine(MoveBoxCoroutine());
+            }
+
+        }
+
+        private IEnumerator MoveBoxCoroutine()
+        {
+            _boxIsMoving = true;
+            Vector3 oldPos;
+            //we stop when this frame would result in a movement past our target
+            while (Mathf.Abs(transform.position.z - _targetDistanceFromPlayer) >= MoveSpeed*Time.deltaTime)
+            {
+                var delta = MoveSpeed*Time.deltaTime;
+                if (transform.position.z > _targetDistanceFromPlayer) delta = -delta;
+                oldPos = transform.position;
+                oldPos.z += delta;
+                transform.position = oldPos;
+                yield return null; //wait for Update
+            }
+            oldPos = transform.position;
+            oldPos.z = _targetDistanceFromPlayer;
+            transform.position = oldPos;
+            _boxIsMoving = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TableDistanceSlider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PsychImmersion.UI
+{
+    [RequireComponent(typeof(Slider))]
+    public class TableDistanceSlider : MonoBehaviour
+    {
+
+        public Text ValueText;
+        private Slider _slider;
+        private float _curValue;
+        private BoxManager _boxManager;
+        public int DecimalPlaces = 1;
+
+        private void Awake()
+        {
+            _slider = GetComponent<Slider>();
+            _slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void Start()
+        {
+            _boxManager = FindObjectOfType<BoxManager>();
+            if (_boxManager == null) {
+                Debug.LogError("Could not find BoxManager");
+                _slider.interactable = false;
+                if (ValueText != null) ValueText.text = "ERR";
+                return;
+            }
+
+            _curValue = (float)Math.Round(_boxManager.DistanceFromPlayer, DecimalPlaces);
+            _slider.value = _curValue;
+            UpdateText();
+        }
+
+        private void OnValueChanged(float newValue)
+        {
+            var rounded = (float)Math.Round(newValue, DecimalPlaces);
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (_curValue == rounded) return;
+            _curValue = rounded;
+            _slider.value = rounded;
+            _boxManager.DistanceFromPlayer = rounded;
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            if (ValueText != null) ValueText.text = string.Format("{0:F1}m", _curValue);
+        }
+
+    }
+}
